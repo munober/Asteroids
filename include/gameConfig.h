fbd01ca4d6792aa9bd1f8c010a5e0dcb932316eb
Teo <teodor.fratiloiu@gmail.com>
2019-12-21 00:19:50 +0200

    Formatting changes

--- include/gameConfig.h
+++ include/gameConfig.h
@@ -9,7 +9,7 @@
 #define INCLUDE_GAMECONFIG_H_
 
 
-#define LASER_BLASTER_SPEED			3
+#define LASER_BLASTER_SPEED			        3
 
 // How close the fired blaster shots have to get to the asteroids to register a hit
 #define HIT_LIMIT_SHOT_SMALL				6
@@ -17,9 +17,9 @@
 #define HIT_LIMIT_SHOT_LARGE				12
 
 // How close the asteroids have to get to the player to register a hit
-#define HIT_LIMIT_SMALL				6
-#define HIT_LIMIT_MEDIUM			8
-#define HIT_LIMIT_LARGE				12
+#define HIT_LIMIT_SMALL				        6
+#define HIT_LIMIT_MEDIUM			        8
+#define HIT_LIMIT_LARGE				        12 
 
 // Score thresholds for going on to the next levels
 // After beating game, will be spawning asteroids to let player go on to reach a new high score
